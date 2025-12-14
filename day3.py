932b63030a5736143f8c5e8ec20c24c3cd423442
Erva <ervservaa@gmail.com>
2025-12-14 16:46:14 +0100

    Day 3 even or odd number

--- day3.py
+++ day3.py
@@ -1,11 +1,5 @@
-score = int (input("Enter score: "))
-if score >=90:
-    print ("A")
-elif score >=80:
-    print ("B")
-elif score >=70:
-    print ("C")
-elif score >=60:
-    print ("D")
+number = int(input("Enter a number:"))
+if number % 2 == 0:
+    print ("Even number")
 else:
-    print ("Grade D or below")
+    print("Odd number")
