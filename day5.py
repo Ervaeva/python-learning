f2ad42428a0f4c1ee4ac3b6a942ac3ee01739e78
Erva <ervservaa@gmail.com>
2025-12-15 20:25:56 +0100

    Day 5 count-even

--- day5.py
+++ day5.py
@@ -1,7 +1,6 @@
-def count_vowels(text):
+def count_even(numbers):
     count = 0
-    for ch in text.lower():
-        if ch in "aeiou":
+    for n in numbers :
+        if n % 2 == 0:
             count += 1
     return count
-
