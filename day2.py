75f9c56edc2a9291bef685afa1c443a039e17e25
Erva <ervservaa@gmail.com>
2025-12-15 19:40:39 +0100

    Upadte day2

--- day2.py
+++ day2.py
@@ -2,4 +2,4 @@ score1= float (input ("Enter score1 : "))
 score2= float(input ("Enter score2: "))
 score3= float(input ("Enter score3:"))
 average= (score1 + score2 + score3)/ 3
-print(f"avreage score = {average}")
+print(f"average score = {average}")
