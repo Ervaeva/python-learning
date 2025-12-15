307f382139e6ef5e0909eb7f8277239a96b9454f
Erva <ervservaa@gmail.com>
2025-12-15 20:05:04 +0100

    Day 4 sum from 1 to n

--- day4.py
+++ day4.py
@@ -1,3 +1,8 @@
-print("Even number 1 - 100")
-for i in range (2, 101, 2):
-   print (i)
+print ("Sum from 1 to n ")
+n = int(input("Ennter a number  "))
+total = 0
+
+for i in range(1, n + 1):
+    total+= i
+print("Sum = ", total)
+
